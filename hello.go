910e134e911cb412febcbc59fbe0fe8c5039d36d
Srikanth <kms100@gmail.com>
2021-10-20 11:57:43 +0530

    Update hello.go

--- hello.go
+++ hello.go
@@ -5,7 +5,7 @@ import "fmt"
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome! We finally met", name)
+	message := fmt.Sprintf("Hi, %v. Welcome! We meet again", name)
 	return message
 }
 
